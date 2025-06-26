117e4b8e3f63cfbee2f89c5469b3d5085268a938
Arham Khurram <arhamkhurram05@gmail.com>
2025-06-26 17:02:37 +0500

    Fixed React Issue for Vercel Deploy

--- frontend/src/context/SocketProvider.tsx
+++ frontend/src/context/SocketProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
+import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
 
 const WS_URL = "ws://localhost:8080";
 
